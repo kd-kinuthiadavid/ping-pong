b7cb7696f76ae7713ac864cdc573e695801cb1c3
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-05-04 16:04:37 +0300

    edit css,html and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,33 @@
-var play = function(userInput){
-    var numberParsed = parseInt(userInput);
-  var numberRange = [];
+function mypingfunction(myresult){
+ var storedarray = [];
 
- for (var i = 1; i <= numberParsed; i++) {
-    if (i % 15 === 0) {
-      numberRange.push("pingpong");
-
-    } else if (i % 3 === 0) {
-      numberRange.push("ping");
-    } else if (i % 5 === 0) {
-      numberRange.push("pong");
-    } else {
-      numberRange.push(i);
-    }
-  }
-
+ for (var i = 1; i <= myresult; i++) {
+   if ((i%3) == 0) {
+     storedarray.push("ping");
+   }else if ((i%5) == 0 ){
+     storedarray.push("pong");
+   }else if ((i%15) == 0) {
+     storedarray.push("pingpong");
+   }else {
+     storedarray.push(i);
+   }
+ }
+// alert(storedarray);
+return storedarray;
 }
+
+
+$(document).ready(function(){
+ $("form#myformid").submit(function(event){
+      // event.preventDefault();
+   $("#answers").empty();
+   var myresult = parseInt($("input#mynumber").val());
+    var storedarray = mypingfunction(myresult);
+    storedarray.forEach(function(myouttput){
+
+      $("#answers").append("<li>" + myouttput + "</li>");
+      event.preventDefault();
+    });
+
+ });
+});
